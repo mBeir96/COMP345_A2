a4d3c7cce13bea040fbd15f9c3929e7efc12242d
riri404 <76566650+riri404@users.noreply.github.com>
2021-11-07 15:48:26 -0500

    Create Player.h

---
+++ Player.h
@@ -0,0 +1,55 @@
+#pragma once
+#include <string>
+#include <vector>
+#include <iostream>
+#include "Orders.h"
+#include "Map.h"
+#include "Cards.h"
+#include "GameEngine.h"
+using namespace std;
+class Territory;
+
+class Player {
+
+public:
+    Player();       // Default constructor
+    Player(string); //added
+    Player(int, string, vector<Territory*> territories, vector<Card*> handCard, vector<Orders*> order); //Constructor
+    Player(const Player& p); //Copy constructor
+    Player& operator = (const Player&); //added
+    ~Player(); //Destructor
+
+    //Methods
+    vector<Territory*> getTerritory();
+    vector<Territory*> toAttack();
+    vector<Territory*> toDefend();
+    vector<Orders*> getOrderList();
+    vector<Card*> getCard();
+    void issueOrder();
+
+    string getName();
+    int getReinforcementPool();
+    void setReinforcementPool(int);
+    void setOrder(Orders*);
+    void setTerritory(Territory);
+    void setName(string);
+    
+    void printOrder();
+    void printHandcard();
+    bool playerContientBouns();
+
+private:
+    string name;
+    int reinforcementPool;
+
+    vector<Territory*> territory;
+    vector<Card*> handCard;
+    vector<Orders*> orderList;
+
+    //friend classes
+    friend class Card;
+    friend class GameEngine;
+    friend class Map;
+};
+
+
