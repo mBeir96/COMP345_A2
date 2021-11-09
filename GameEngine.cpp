47602490c6007608574d6d4b36ed136b35abd677
mBeir <beiro1996@gmail.com>
2021-11-09 14:06:33 -0500

    game engine file h, cpp and driver.

---
+++ GameEngine.cpp
@@ -0,0 +1,72 @@
+#include "GameEngine.h"
+#include <iostream>
+#include <string>
+
+
+GameEngine::GameEngine()
+{
+    this->StartUp = false;
+    this->Play = false;
+}
+
+GameEngine::GameEngine(bool StartUp, bool Play)
+{
+    this->StartUp = StartUp;
+    this->Play = Play;
+}
+
+GameEngine::GameEngine(const GameEngine& g)
+{
+    StartUp = new bool(g.StartUp);
+    Play = new bool(g.Play);
+}
+
+GameEngine::~GameEngine()
+{
+    std::cout << "The object is destroy..." << std::endl;
+}
+
+GameEngine& GameEngine::operator=(const GameEngine& g)
+{
+    if (this == &g)
+	{
+    	return *this;
+    }
+    StartUp = new bool(g.StartUp);
+    Play = new bool(g.Play);
+
+    return *this;
+}
+
+std::ostream& operator<<(std::ostream& out, const GameEngine& g)
+{
+    out << g.StartUp << " " << g.Play << std::endl;
+    return out;
+}
+
+std::istream& operator>>(std::istream& in, GameEngine& g)
+{
+    return in;
+}
+
+void GameEngine::loadMap() {
+	//add code here later 
+}
+void GameEngine::validateMap() {
+	//add code here later 
+}
+void GameEngine::addPlayer() {
+	//add code here later 
+}
+void GameEngine::issueOrder() {
+	//add code here later 
+}
+void GameEngine::executeOrder() {
+	//add code here later 
+}
+void GameEngine::endExecuteOrder(){
+	//add code here later 
+}
+void GameEngine::win() {
+	//add code here later 
+}
---
+++ GameEngine.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <iostream>
+#include <string>
+enum RiskState {
+	START, MAPLOADED, MAPVALIDATED, PLAYERSADDED, ASSIGNMENTREIFORCEMENT, ISSUEORDERS, EXECUTEORDERS, WIN
+};
+class GameEngine {
+	public:
+		GameEngine();
+		GameEngine(bool, bool);
+		GameEngine(const GameEngine &g);
+		GameEngine& operator=(const GameEngine& g);
+        friend std::istream& operator>>(std::istream& in, GameEngine& g);
+        friend std::ostream& operator<<(std::ostream &out, const GameEngine &g);
+        ~GameEngine();
+		void loadMap();
+		void validateMap();
+		void addPlayer();
+		void issueOrder();
+		void executeOrder();
+		void endExecuteOrder();
+		void win();
+		
+	private:
+		bool StartUp;
+		bool Play;
+	
+};
