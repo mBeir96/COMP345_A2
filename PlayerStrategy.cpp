80a98cb4b7f2d679f0238d2bc729d3b86f4dbe70
riri404 <76566650+riri404@users.noreply.github.com>
2021-12-03 16:37:07 -0500

    Add files via upload

---
+++ PlayerStrategy.cpp
@@ -0,0 +1,94 @@
+#include "PlayerStrategy.h"
+
+
+int HumanPlayerStrategy::changeStrategy(string order, int armies)
+{
+	cout << "HumanPlayerStrategy: Please choose a strategy from the following \n"
+		"1 for issueOrder(), 2 for toAttack(), 3 for toDefend(). " << endl;
+	int input;
+	cin >> input;
+	if (input == 1) {
+		issueOrder(order);
+		return armies - 1;
+	}
+	else if (input == 2) {
+		toAttack();
+		return armies - 2;
+	}
+	else if (input == 3) {
+		toDenfend();
+		return armies;
+	}
+	else {
+		cout << "Please input an correct integer from 1 to 3...\n" << endl;
+	}
+}
+
+void HumanPlayerStrategy::issueOrder(string order)
+{
+	cout << "HumanPlayerStrategy: issueOrder() -> " << order << endl;
+}
+
+void HumanPlayerStrategy::toAttack()
+{
+	cout << "HumanPlayerStrategy: toAttack() " << endl;
+}
+
+void HumanPlayerStrategy::toDenfend()
+{
+	cout << "HumanPlayerStrategy: toAttack() " << endl;
+}
+
+
+
+int AggressivePlayerStrategy::changeStrategy(string order, int armies)
+{
+	cout << "AggressivePlayerStrategy: makeDecision() " << endl;
+	
+	issueOrder(order);
+	
+	toAttack();
+
+	toDenfend();
+	return armies - 5;
+}
+
+void AggressivePlayerStrategy::issueOrder(string order)
+{
+	cout << "AggressivePlayerStrategy: issueOrder() -> " << order << " -> reinforces its strongest country. " << endl;
+}
+
+void AggressivePlayerStrategy::toAttack()
+{
+	cout << "AggressivePlayerStrategy: toAttack() " << endl;
+}
+
+void AggressivePlayerStrategy::toDenfend()
+{
+	cout << "AggressivePlayerStrategy: toDenfend() -> maximize aggregation of forces in the country" << endl;
+}
+
+int BenevolentPlayerStrategy::changeStrategy(string order, int numArmy)
+{
+	cout << "BenevolentPlayerStrategy: makeDecision() " << endl;
+	//focus on protecting its weak country
+	issueOrder(order);
+	//never attacks, toDefend
+	toDenfend();
+	return numArmy - 1;
+}
+
+void BenevolentPlayerStrategy::issueOrder(string order)
+{
+	cout << "BenevolentPlayerStrategy: issueOrder() -> " << order << " -> reinforces its weakest country. " << endl;
+}
+
+void BenevolentPlayerStrategy::toAttack()
+{
+	cout << "BenevolentPlayerStrategy : never attacks" << endl;
+}
+
+void BenevolentPlayerStrategy::toDenfend()
+{
+	cout << "BenevolentPlayerStrategy: toDenfend() -> move armies to weaker countries. " << endl;
+}
---
+++ PlayerStrategy.h
@@ -0,0 +1,48 @@
+#pragma once
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include "Player.h"
+
+//abstract Playerstrategy class
+class PlayerStrategy
+{
+	//create method that will change strategy in the isuueing orders phase
+	virtual int changeStrategy(string, int);
+	//implement issueOrder(), toAttack(), toDefend() in different ConcreteStrategy 
+	virtual void issueOrder(string) = 0;
+	virtual void toAttack() = 0;
+	virtual void toDenfend() = 0;
+
+};
+
+//ConcreteStrategy classes
+class HumanPlayerStrategy: public PlayerStrategy {
+
+	int changeStrategy(string, int);
+	//implement issueOrder(), toAttack(), toDefend() 
+	void issueOrder(string) = 0;
+	void toAttack() = 0;
+    void toDenfend() = 0;
+};
+
+class AggressivePlayerStrategy : public PlayerStrategy {
+	int changeStrategy(string, int);
+	//implement issueOrder(), toAttack(), toDefend() 
+	void issueOrder(string) = 0;
+	void toAttack() = 0;
+	void toDenfend() = 0;
+};
+
+class BenevolentPlayerStrategy : public PlayerStrategy {
+	int changeStrategy(string, int);
+	//implement issueOrder(), toAttack(), toDefend() 
+	void issueOrder(string) = 0;
+	void toAttack() = 0;
+	void toDenfend() = 0;
+};
+
+
+
+
